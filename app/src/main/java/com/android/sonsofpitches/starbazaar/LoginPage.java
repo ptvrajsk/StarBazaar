ff3eb87174c0c756108b4c0c2bbdf3fe6d0add0b
Prithivi Raj <ptvrajsk@gmail.com>
2018-06-06 14:42:07 +0800

    Changed the Min SDK Version back to 19 because I figured another way to get the background images and what not by using the SVG (Scalable Vector Graphics). Imported a couple of the Assets and assigned them. Mainly to the UserButton, OrganizationButton (OrgButton), and LoginPageBackground. To change any of the Assets simply edit them on Figma and then re-import into Android Studio (this will replace the old assets with the new one).

--- app/src/main/java/com/android/sonsofpitches/starbazaar/LoginPage.java
+++ app/src/main/java/com/android/sonsofpitches/starbazaar/LoginPage.java
@@ -1,7 +1,9 @@
 package com.android.sonsofpitches.starbazaar;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        ConstraintLayout loginPageView = findViewById(R.id.loginPage);
+        Button userButton = findViewById(R.id.userButton);
+        Button orgButton = findViewById(R.id.orgButton);
+
+        loginPageView.setBackgroundResource(R.drawable.loginpagebackground);
+        userButton.setBackgroundResource(R.drawable.loginpage_userbutton_background);
+        orgButton.setBackgroundResource(R.drawable.loginpage_orgbutton_background);
+
     }
 }
