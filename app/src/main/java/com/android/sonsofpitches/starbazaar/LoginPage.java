0ebade2942843ca98d8664c637db7b7f51d47fc4
Prithivi Raj <ptvrajsk@gmail.com>
2018-06-06 22:42:12 +0800

    Replaced the vector assets for LoginPage buttons because the bold fonts looked extremely ugly. Added the blinking stars effect to make the starting page more interactive.

--- app/src/main/java/com/android/sonsofpitches/starbazaar/LoginPage.java
+++ app/src/main/java/com/android/sonsofpitches/starbazaar/LoginPage.java
@@ -4,7 +4,10 @@ import android.annotation.SuppressLint;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -15,13 +18,41 @@ public class LoginPage extends AppCompatActivity {
         setContentView(R.layout.activity_login_page);
 
         ConstraintLayout loginPageView = findViewById(R.id.loginPage);
+
         Button userButton = findViewById(R.id.userButton);
         Button orgButton = findViewById(R.id.orgButton);
 
-        loginPageView.setBackgroundResource(R.drawable.svg_loginpagebackground);
+        loginPageView.setBackgroundResource(R.drawable.svg_loginpage_background);
         userButton.setBackgroundResource(R.drawable.loginpage_userbutton_background);
         orgButton.setBackgroundResource(R.drawable.loginpage_orgbutton_background);
 
+        final ImageView stars = findViewById(R.id.stars);
+        final Animation animationFadeIn = AnimationUtils.loadAnimation(this, R.anim.fadein);
+        final Animation animationFadeOut = AnimationUtils.loadAnimation(this, R.anim.fadeout);
+        Animation.AnimationListener animationListener = new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                //Do nothing
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                if (animation == animationFadeIn) {
+                    stars.startAnimation(animationFadeOut);
+                } else {
+                    stars.startAnimation(animationFadeIn);
+                }
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+                //Do nothing
+            }
+        };
+
+        animationFadeIn.setAnimationListener(animationListener);
+        animationFadeOut.setAnimationListener(animationListener);
+        stars.startAnimation(animationFadeIn);
 
     }
 }
