35657e9faaa5cc9f1bf0fc684f116859de772ef7
Prithivi Raj <ptvrajsk@gmail.com>
2018-06-06 16:12:20 +0800

    Added the remaining resources for the login page (StarBazaar title). Changed the scripting from a single ConstraintLayout to 2 nested Constraint Layouts so that it was easier to organize and arrange all the View Elements into their respective places. Basic Login Page is now completed.
    UI Scripted.
    All SVGs imported and assigned.
    Button Animations Done.
    
    Once the other activities are designed, can begin linking the buttons to
    the respective activities.

--- app/src/main/java/com/android/sonsofpitches/starbazaar/LoginPage.java
+++ app/src/main/java/com/android/sonsofpitches/starbazaar/LoginPage.java
@@ -1,5 +1,6 @@
 package com.android.sonsofpitches.starbazaar;
 
+import android.annotation.SuppressLint;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.widget.Button;
 
 public class LoginPage extends AppCompatActivity {
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,9 +18,10 @@ public class LoginPage extends AppCompatActivity {
         Button userButton = findViewById(R.id.userButton);
         Button orgButton = findViewById(R.id.orgButton);
 
-        loginPageView.setBackgroundResource(R.drawable.loginpagebackground);
+        loginPageView.setBackgroundResource(R.drawable.svg_loginpagebackground);
         userButton.setBackgroundResource(R.drawable.loginpage_userbutton_background);
         orgButton.setBackgroundResource(R.drawable.loginpage_orgbutton_background);
+
 
     }
 }
