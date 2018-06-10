9aff61ffac713b053d6562a98fd39ac0adc0f8a3
Prithivi Raj <ptvrajsk@gmail.com>
2018-06-10 13:40:48 +0800

    Header xml scripted and java class tied to the header created.

---
+++ app/src/main/java/com/android/sonsofpitches/starbazaar/HeaderFragment.java
@@ -0,0 +1,21 @@
+package com.android.sonsofpitches.starbazaar;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HeaderFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+
+        View view = inflater.inflate(R.layout.header_fragment, container, false);
+    }
+}
