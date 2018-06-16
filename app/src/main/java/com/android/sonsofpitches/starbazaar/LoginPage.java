bed803255243701a55a6144b984d8c7b3f4626ca
Prithivi Raj <ptvrajsk@gmail.com>
2018-06-16 23:03:03 +0800

    CardView Prefab for each event has been scripted without any logic implementation

--- app/src/main/java/com/android/sonsofpitches/starbazaar/LoginPage.java
+++ app/src/main/java/com/android/sonsofpitches/starbazaar/LoginPage.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.EventLog;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
