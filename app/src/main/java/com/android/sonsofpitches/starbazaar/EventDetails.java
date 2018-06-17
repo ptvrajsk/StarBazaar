2fe4e6cb9979dad49e8b68018a45411bb22675aa
Ploty Gotty <me@dingdong.com>
2018-06-17 18:18:50 +0800

    added in the button stuff. the scroll view i know how to do it but somehow when i implement the layout fucks up

--- app/src/main/java/com/android/sonsofpitches/starbazaar/EventDetails.java
+++ app/src/main/java/com/android/sonsofpitches/starbazaar/EventDetails.java
@@ -1,7 +1,12 @@
 package com.android.sonsofpitches.starbazaar;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.content.Intent;
+import android.view.View;
+
 
 public class EventDetails extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class EventDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
+
+        RelativeLayout eventOverview = findViewById(R.id.eventOverview);
+        eventOverview.setBackgroundResource(R.drawable.svg_loginpage_background);
+    }
+
+    public void onClickShop (View view) {
+        Intent goToShop = new Intent(this, IndividualShopDetails.class);
+        startActivity(goToShop);
+    }
+
+    // TODO: Fill in the events list class name (Back button functionality?)
+    public void backButton (View view) {
+        Intent goBackToEvents = new Intent(this,LoginPage.class ); // FILL IN EVENTS LIST CLASS INSTEAD. JUST PUT THIS AS A
+                                                                                // FILLER FOR THE TIME BEING.
+        startActivity(goBackToEvents);
     }
 }
