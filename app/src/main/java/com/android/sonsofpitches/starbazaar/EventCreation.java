faaabe1cef2e38eee16428b2baebcd6418404ebe
Prithivi Raj <ptvrajsk@gmail.com>
2018-06-29 13:45:20 +0800

    Basic Event Creator activity scripted, moving on to create SQL Database to store information

--- app/src/main/java/com/android/sonsofpitches/starbazaar/EventCreation.java
+++ app/src/main/java/com/android/sonsofpitches/starbazaar/EventCreation.java
@@ -1,6 +1,7 @@
 package com.android.sonsofpitches.starbazaar;
 
 import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -11,6 +12,10 @@ public class EventCreation extends AppCompatActivity implements HeaderFragment.H
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_creation);
+
+        ConstraintLayout creationActivity = findViewById(R.id.eventCreationActivity);
+        creationActivity.setBackgroundResource(R.drawable.svg_loginpage_background);
+
     }
 
     @Override
